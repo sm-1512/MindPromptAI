38ed56468cd2945f77fa5f942f2809d07e29a307
Sahil Mishra <sahil.mishra1512@gmail.com>
2025-08-17 13:40:24 +0530

    Blog Titles & Image Generation Implemented

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,13 +1,13 @@
 import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
-
+import axios from "axios";
+import { v2 as cloudinary } from "cloudinary";
 
 const AI = new OpenAI({
-    apiKey: process.env.GEMINI_API_KEY,
-    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+  apiKey: process.env.GEMINI_API_KEY,
+  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
 });
-
 
 export const generateArticle = async (req, res) => {
   try {
@@ -55,7 +55,110 @@ export const generateArticle = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Something went wrong while generating the article.",
-      error: error.message, 
+      error: error.message,
+    });
+  }
+};
+
+export const generateBlogTitle = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt } = req.body;
+    const { plan, free_usage } = req; // values set in your auth middleware
+
+    // 1. Check usage limits for free users
+    if (plan !== "premium" && free_usage >= 10) {
+      return res.status(403).json({
+        success: false,
+        message: "Free usage limit reached. Upgrade to premium to continue.",
+      });
+    }
+
+    // 2. Call AI model to generate article
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 0.7,
+      max_tokens: 100,
+    });
+
+    const content = response.choices[0].message.content;
+
+    // 3. Save creation to DB
+    await sql`
+      INSERT INTO creations (user_id, prompt, content, type) 
+      VALUES (${userId}, ${prompt}, ${content}, 'blog-title')
+    `;
+
+    // 4. Update free usage count if not premium
+    if (plan !== "premium") {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: {
+          free_usage: free_usage + 1,
+        },
+      });
+    }
+
+    // 5. Send response
+    return res.status(200).json({ success: true, content });
+  } catch (error) {
+    console.error("generateTitle error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while generating the blog title.",
+      error: error.message,
+    });
+  }
+};
+
+export const generateImage = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { prompt, publish } = req.body;
+    const { plan } = req; // values set in your auth middleware
+
+    //No free_usage variables because it is only available for premium users
+
+    // 1. Check usage limits for free users
+    if (plan !== "premium") {
+      return res.status(403).json({
+        success: false,
+        message: "This feature is only available for premium users.",
+      });
+    }
+
+    // 2. Call AI model to generate article
+    const formData = new FormData();
+    formData.append("prompt", prompt);
+    const { data } = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formData,
+      {
+        headers: { "x-api-key": process.env.CLIPDROP_API_KEY },
+        responseType: "arraybuffer",
+      }
+    );
+    const base64Image = `data:image/png;base64,${Buffer.from(
+      data,
+      "binary"
+    ).toString("base64")}`;
+
+    const {secure_url} = await cloudinary.uploader.upload(base64Image);
+
+    // 3. Save creation to DB
+    await sql`
+      INSERT INTO creations (user_id, prompt, content, type, publish) 
+      VALUES (${userId}, ${prompt}, ${secure_url}, 'image', ${publish ?? false})
+    `;
+
+    // 4. Send response
+    return res.status(200).json({ success: true, secure_url });
+  } catch (error) {
+    console.error("generateTitle error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while generating the image.",
+      error: error.message,
     });
   }
 };
